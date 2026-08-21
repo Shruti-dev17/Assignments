6aa13276e8310131db26b65ccf33f8da89ad1f14
Shruti-dev17 <sq9787@gmail.com>
2026-08-22 02:27:44 +0530

    update

--- python_Assignment/Files & Exceptions/read_handle-errors.py
+++ python_Assignment/Files & Exceptions/read_handle-errors.py
@@ -4,19 +4,19 @@
 
 # opening the file in read mode and using try-except method for handling error
 try:
-    fh = open(r'Python_Assignment\Assignment 4\sample.txt','r') 
+    with open(r'F:\Desktop\code\Assignment\python_Assignment\Files & Exceptions\sample.txt','r') as fh:
+        read_file= fh.readlines() # using readlines() to read the content of file line by line
+        count = 1 
+        
+        for line in read_file:
+            print(f"line {count}: {line}")
+            count+=1
+        
+        fh.close() # closing the file.
+        
 
 except FileNotFoundError :
    print("The file sample.txt was not found")
 
-# else is used to run code after error is validated and no error is found
-else:
-    read_file= fh.readlines() # using readlines() to read the content of file line by line
-    count = 1 
 
-    for line in read_file:
-        print(f"line {count}: {line}")
-        count+=1
-
-    fh.close() # closing the file.
-
+    
